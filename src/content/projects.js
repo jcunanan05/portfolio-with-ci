dcea6baa5e5ff390d4e35792c6c05b9deb28f35e
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-12-18 16:33:59 -0500

    Temporarily hardcode project data.
    Will get back later to fetch in contentful.

---
+++ src/content/projects.js
@@ -0,0 +1,291 @@
+export default {
+  "sys": {
+    "type": "Array"
+  },
+  "total": 3,
+  "skip": 0,
+  "limit": 100,
+  "items": [
+    {
+      "sys": {
+        "space": {
+          "sys": {
+            "type": "Link",
+            "linkType": "Space",
+            "id": "dedwkncyi8fn"
+          }
+        },
+        "id": "25T67Fettik8qUcAgKWS6I",
+        "type": "Entry",
+        "createdAt": "2018-10-31T18:55:36.608Z",
+        "updatedAt": "2018-11-12T19:38:17.147Z",
+        "environment": {
+          "sys": {
+            "id": "master",
+            "type": "Link",
+            "linkType": "Environment"
+          }
+        },
+        "revision": 4,
+        "contentType": {
+          "sys": {
+            "type": "Link",
+            "linkType": "ContentType",
+            "id": "posts"
+          }
+        },
+        "locale": "en-US"
+      },
+      "fields": {
+        "type": "project",
+        "title": "Community Page",
+        "url": "https://stepupnow.herokuapp.com/",
+        "codeUrl": "https://github.com/jcunanan05/step-up",
+        "content": "Made a website for a new Community in Weston. Did the front-end, back-end and CMS integration.",
+        "picture": {
+          "sys": {
+            "space": {
+              "sys": {
+                "type": "Link",
+                "linkType": "Space",
+                "id": "dedwkncyi8fn"
+              }
+            },
+            "id": "XLjwGx11C0SQuyQOsU6YM",
+            "type": "Asset",
+            "createdAt": "2018-10-31T18:54:20.591Z",
+            "updatedAt": "2018-10-31T18:54:20.591Z",
+            "environment": {
+              "sys": {
+                "id": "master",
+                "type": "Link",
+                "linkType": "Environment"
+              }
+            },
+            "revision": 1,
+            "locale": "en-US"
+          },
+          "fields": {
+            "title": "Step Up Now",
+            "description": "Step Up Now Website Screenshot",
+            "file": {
+              "url": "//images.ctfassets.net/dedwkncyi8fn/XLjwGx11C0SQuyQOsU6YM/9aed764302ca24641953d4e2f35b78dc/step-up.png",
+              "details": {
+                "size": 48794,
+                "image": {
+                  "width": 2478,
+                  "height": 1406
+                }
+              },
+              "fileName": "step-up.png",
+              "contentType": "image/png"
+            }
+          }
+        },
+        "tags": "react,node,express,contentful-cms"
+      }
+    },
+    {
+      "sys": {
+        "space": {
+          "sys": {
+            "type": "Link",
+            "linkType": "Space",
+            "id": "dedwkncyi8fn"
+          }
+        },
+        "id": "3olFY7ZTLq2CEcC2oWWQUC",
+        "type": "Entry",
+        "createdAt": "2018-10-31T19:03:21.991Z",
+        "updatedAt": "2018-11-12T19:39:02.503Z",
+        "environment": {
+          "sys": {
+            "id": "master",
+            "type": "Link",
+            "linkType": "Environment"
+          }
+        },
+        "revision": 4,
+        "contentType": {
+          "sys": {
+            "type": "Link",
+            "linkType": "ContentType",
+            "id": "posts"
+          }
+        },
+        "locale": "en-US"
+      },
+      "fields": {
+        "type": "project",
+        "title": "Markdown Previewer",
+        "url": "https://jcunanan05-markdown-preview.netlify.com/",
+        "codeUrl": "https://github.com/jcunanan05/markdown-preview",
+        "content": "Markdown Previewer to help you visualize what markdown will look like in HTML. Markdown text is used for easier content writing.",
+        "picture": {
+          "sys": {
+            "space": {
+              "sys": {
+                "type": "Link",
+                "linkType": "Space",
+                "id": "dedwkncyi8fn"
+              }
+            },
+            "id": "HI7HYNEbAcqiKysoUomsm",
+            "type": "Asset",
+            "createdAt": "2018-10-31T19:00:59.674Z",
+            "updatedAt": "2018-10-31T19:00:59.674Z",
+            "environment": {
+              "sys": {
+                "id": "master",
+                "type": "Link",
+                "linkType": "Environment"
+              }
+            },
+            "revision": 1,
+            "locale": "en-US"
+          },
+          "fields": {
+            "title": "markdown",
+            "description": "Markdown previewer Screenshot",
+            "file": {
+              "url": "//images.ctfassets.net/dedwkncyi8fn/HI7HYNEbAcqiKysoUomsm/edb70496716a55450000e82be5adb4fa/markdown.png",
+              "details": {
+                "size": 195957,
+                "image": {
+                  "width": 2478,
+                  "height": 1406
+                }
+              },
+              "fileName": "markdown.png",
+              "contentType": "image/png"
+            }
+          }
+        },
+        "tags": "react,markdown"
+      }
+    },
+    {
+      "sys": {
+        "space": {
+          "sys": {
+            "type": "Link",
+            "linkType": "Space",
+            "id": "dedwkncyi8fn"
+          }
+        },
+        "id": "39ArRcXinKwoWWywaeGyiW",
+        "type": "Entry",
+        "createdAt": "2018-12-18T20:05:51.056Z",
+        "updatedAt": "2018-12-18T20:15:02.976Z",
+        "environment": {
+          "sys": {
+            "id": "master",
+            "type": "Link",
+            "linkType": "Environment"
+          }
+        },
+        "revision": 3,
+        "contentType": {
+          "sys": {
+            "type": "Link",
+            "linkType": "ContentType",
+            "id": "posts"
+          }
+        },
+        "locale": "en-US"
+      },
+      "fields": {
+        "type": "project",
+        "title": "Myfuturesaver.org",
+        "url": "https://myfuturesaver.org",
+        "codeUrl": "https://github.com/jcunanan05/myfuturesaver",
+        "content": "Made a landing page for SmartSAVER.org's application. Combined their 3rd-party services to one page so application can be more fluid.",
+        "tags": "express,node,aws,nunjucks"
+      }
+    }
+  ],
+  "includes": {
+    "Asset": [
+      {
+        "sys": {
+          "space": {
+            "sys": {
+              "type": "Link",
+              "linkType": "Space",
+              "id": "dedwkncyi8fn"
+            }
+          },
+          "id": "HI7HYNEbAcqiKysoUomsm",
+          "type": "Asset",
+          "createdAt": "2018-10-31T19:00:59.674Z",
+          "updatedAt": "2018-10-31T19:00:59.674Z",
+          "environment": {
+            "sys": {
+              "id": "master",
+              "type": "Link",
+              "linkType": "Environment"
+            }
+          },
+          "revision": 1,
+          "locale": "en-US"
+        },
+        "fields": {
+          "title": "markdown",
+          "description": "Markdown previewer Screenshot",
+          "file": {
+            "url": "//images.ctfassets.net/dedwkncyi8fn/HI7HYNEbAcqiKysoUomsm/edb70496716a55450000e82be5adb4fa/markdown.png",
+            "details": {
+              "size": 195957,
+              "image": {
+                "width": 2478,
+                "height": 1406
+              }
+            },
+            "fileName": "markdown.png",
+            "contentType": "image/png"
+          }
+        }
+      },
+      {
+        "sys": {
+          "space": {
+            "sys": {
+              "type": "Link",
+              "linkType": "Space",
+              "id": "dedwkncyi8fn"
+            }
+          },
+          "id": "XLjwGx11C0SQuyQOsU6YM",
+          "type": "Asset",
+          "createdAt": "2018-10-31T18:54:20.591Z",
+          "updatedAt": "2018-10-31T18:54:20.591Z",
+          "environment": {
+            "sys": {
+              "id": "master",
+              "type": "Link",
+              "linkType": "Environment"
+            }
+          },
+          "revision": 1,
+          "locale": "en-US"
+        },
+        "fields": {
+          "title": "Step Up Now",
+          "description": "Step Up Now Website Screenshot",
+          "file": {
+            "url": "//images.ctfassets.net/dedwkncyi8fn/XLjwGx11C0SQuyQOsU6YM/9aed764302ca24641953d4e2f35b78dc/step-up.png",
+            "details": {
+              "size": 48794,
+              "image": {
+                "width": 2478,
+                "height": 1406
+              }
+            },
+            "fileName": "step-up.png",
+            "contentType": "image/png"
+          }
+        }
+      }
+    ]
+  }
+}
